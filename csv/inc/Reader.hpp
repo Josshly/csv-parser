e038d14957beb0216d842cc7f4de2e005c5a9965
Joshly <none@none.com>
2026-09-07 18:40:35 +0300

    Add parameters.hpp

--- csv/inc/Reader.hpp
+++ csv/inc/Reader.hpp
@@ -6,33 +6,9 @@
 #include <string>
 #include <vector>
 
+#include "parameters.hpp"
+
 namespace csv {
-
-template <char ch>
-struct delimiter {
-  static constexpr char value = ch;
-};
-
-template <char ch>
-struct quoteCharacter {
-  static constexpr char value = ch;
-};
-
-struct trim_none {
-  static void trim(std::string&) noexcept {}
-};
-
-struct trim_whitespace {
-  static void trim(std::string& s) {
-    auto start = s.find_first_not_of(" \t\r\n");
-    if (start == std::string::npos) {
-      s.clear();
-      return;
-    }
-    auto end = s.find_last_not_of(" \t\r\n");
-    s = s.substr(start, end - start + 1);
-  }
-};
 
 template <typename Delimiter = delimiter<','>,
           typename QuoteChar = quoteCharacter<'"'>,
---
+++ csv/inc/parameters.hpp
@@ -0,0 +1,36 @@
+#ifndef CSV_INC_PARAMETERS_HPP
+#define CSV_INC_PARAMETERS_HPP
+#include <string>
+
+template <bool b>
+struct first_row_is_header {
+  static constexpr bool value = b;
+};
+
+template <char ch>
+struct delimiter {
+  static constexpr char value = ch;
+};
+
+template <char ch>
+struct quoteCharacter {
+  static constexpr char value = ch;
+};
+
+struct trim_none {
+  static void trim(std::string&) noexcept {}
+};
+
+struct trim_whitespace {
+  static void trim(std::string& s) {
+    auto start = s.find_first_not_of(" \t\r\n");
+    if (start == std::string::npos) {
+      s.clear();
+      return;
+    }
+    auto end = s.find_last_not_of(" \t\r\n");
+    s = s.substr(start, end - start + 1);
+  }
+};
+
+#endif
