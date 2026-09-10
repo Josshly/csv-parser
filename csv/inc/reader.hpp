1c60f7b265644b09aaad6d23738a64ee0ccf0e17
Joshly <none@none.com>
2026-09-10 18:34:46 +0300

    change definition of the Reader class methods to be outside the class

--- csv/inc/reader.hpp
+++ csv/inc/reader.hpp
@@ -18,69 +18,77 @@ class Reader {
   Reader() : input(nullptr) {}
   explicit Reader(std::istream& is) : input(&is) {}
 
-  bool getRow(std::vector<std::string>& output) {
-    if (input == nullptr) {
-      throw std::runtime_error("csv::Reader::getRow: no input stream attached");
-    }
-    output.clear();
-    std::string current_field;
-    bool in_quotes = false;
-    bool got_anything = false;
-    char c;
-    constexpr char quote_char = QuoteChar::value;
-    constexpr char delim = Delimiter::value;
+  bool getRow(std::vector<std::string>& output);
 
-    while (input->get(c)) {
-      got_anything = true;
-      if (c == '\r') {
-        continue;
-      }
-      if (in_quotes) {
-        if (c == quote_char) {
-          if (input->peek() == quote_char) {
-            input->get(c);
-            current_field += quote_char;
-          } else {
-            in_quotes = false;
-          }
-        } else {
-          current_field += c;
-        }
-      } else {
-        if (c == quote_char) {
-          in_quotes = true;
-        } else if (c == delim) {
-          TrimPolicy::trim(current_field);
-          output.push_back(std::move(current_field));
-          current_field.clear();
-        } else if (c == '\n') {
-          TrimPolicy::trim(current_field);
-          output.push_back(std::move(current_field));
-          return true;
-        } else {
-          current_field += c;
-        }
-      }
-    }
-
-    if (got_anything) {
-      TrimPolicy::trim(current_field);
-      output.push_back(std::move(current_field));
-      return true;
-    }
-    return false;
-  }
-
-  void parse(std::vector<std::vector<std::string>>& output) {
-    std::vector<std::string> row;
-    while (getRow(row)) {
-      output.push_back(std::move(row));
-    }
-  }
+  void parse(std::vector<std::vector<std::string>>& output);
 
  private:
   std::istream* input;
 };
+
+template <typename Delimiter, typename QuoteChar, typename TrimPolicy>
+bool Reader<Delimiter, QuoteChar, TrimPolicy>::getRow(
+    std::vector<std::string>& output) {
+  if (input == nullptr) {
+    throw std::runtime_error("csv::Reader::getRow: no input stream attached");
+  }
+  output.clear();
+  std::string current_field;
+  bool in_quotes = false;
+  bool got_anything = false;
+  char c;
+  constexpr char quote_char = QuoteChar::value;
+  constexpr char delim = Delimiter::value;
+
+  while (input->get(c)) {
+    got_anything = true;
+    if (c == '\r') {
+      continue;
+    }
+    if (in_quotes) {
+      if (c == quote_char) {
+        if (input->peek() == quote_char) {
+          input->get(c);
+          current_field += quote_char;
+        } else {
+          in_quotes = false;
+        }
+      } else {
+        current_field += c;
+      }
+    } else {
+      if (c == quote_char) {
+        in_quotes = true;
+      } else if (c == delim) {
+        TrimPolicy::trim(current_field);
+        output.push_back(std::move(current_field));
+        current_field.clear();
+      } else if (c == '\n') {
+        TrimPolicy::trim(current_field);
+        output.push_back(std::move(current_field));
+        return true;
+      } else {
+        current_field += c;
+      }
+    }
+  }
+
+  if (got_anything) {
+    TrimPolicy::trim(current_field);
+    output.push_back(std::move(current_field));
+    return true;
+  }
+  return false;
+}
+
+template <typename Delimiter, typename QuoteChar, typename TrimPolicy>
+void Reader<Delimiter, QuoteChar, TrimPolicy>::parse(
+    std::vector<std::vector<std::string>>& output) {
+  std::vector<std::string> row;
+  while (getRow(row)) {
+    output.push_back(std::move(row));
+  }
+}
 
 }  // namespace csv
 
